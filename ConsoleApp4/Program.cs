7b7aaeda962015df79035a838f1aa7a6cbaf81a0
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 13:35:32 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace constantapp
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.141;
+
+            double r;
+            Console.WriteLine("enter radius:");
+            r = Convert.ToDouble(Console.ReadLine());
+            double areaofcircle = pi * r * r;
+            Console.WriteLine("r={0}, area of the circle={1}", r, areaofcircle);
+            Console.ReadLine();
+        }
+    }
+}
